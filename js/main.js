4714c4b93f64cb3aba81f78cb90a4376f36cf3db
Gerwin Beumer <gerwin@debeumers.nl>
2013-06-25 13:25:56 +0200

    Campagne add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+	/*var notliked = (querystring('notliked')=="1") ? true : false;;
+
+	if (notliked){
+		$('.header-container #like').show();
+	}
+
+	function querystring(key) {
+	   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+	   var r=[], m;
+	   while ((m=re.exec(document.location.search)) != null) r.push(m[1]);
+	   return r;
+	}*/
+
+});
